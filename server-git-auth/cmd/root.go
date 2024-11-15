e77244ab85ccb4bfacd147566a3b0f1216a0eec7
dane <danerieber@gmail.com>
2024-11-15 00:20:46 -0600

    change permissions of socket

--- server-git-auth/cmd/root.go
+++ server-git-auth/cmd/root.go
@@ -57,6 +57,10 @@ var rootCmd = &cobra.Command{
 			}
 			log.Printf("listening at %v", listener.Addr())
 
+			if err := os.Chmod(socket, 0666); err != nil {
+				log.Fatal(err)
+			}
+
 			// clean up socket file
 			c := make(chan os.Signal, 1)
 			signal.Notify(c, os.Interrupt, syscall.SIGTERM)
