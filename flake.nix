9590bb44f6edbdc043a5b2b01b3faaa153e7ad7d
Dane Rieber <danerieber@gmail.com>
2024-11-21 17:49:42 -0600

    nix package for cli

--- flake.nix
+++ flake.nix
@@ -9,6 +9,7 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        vendorHash = "sha256-KK8Q3SMK3KSrCsHpdW4sxKNTSNGi0UW0YhsCaQcRxhc=";
       in
       {
         devShell = pkgs.mkShell {
@@ -26,6 +27,37 @@
             protoc-gen-go
             protoc-gen-go-grpc
           ];
+        };
+        packages = {
+          default =
+            let
+              version = "0.1";
+              base = pkgs.buildGoModule {
+                inherit version vendorHash;
+                pname = "nf6-cli";
+                src = ./.;
+                subPackages = [ "cli" ];
+              };
+            in
+            pkgs.stdenv.mkDerivation {
+              inherit version;
+              pname = "nf";
+              nativeBuildInputs = [
+                base
+                pkgs.makeBinaryWrapper
+                pkgs.installShellFiles
+              ];
+              dontUnpack = true;
+              installPhase = ''
+                mkdir -p $out/bin
+                makeBinaryWrapper ${base}/bin/cli $out/bin/nf \
+                  --prefix PATH : ${with pkgs; lib.makeBinPath [ openssh ]}
+                installShellCompletion --cmd nf \
+                  --bash <($out/bin/nf completion bash) \
+                  --fish <($out/bin/nf completion fish) \
+                  --zsh  <($out/bin/nf completion zsh)
+              '';
+            };
         };
       }
     );
