0d8c9a8bfefd40b788415a392f7078161735e307
Dane Rieber <danerieber@gmail.com>
2024-11-25 13:17:53 -0600

    update iso flake

--- iso/flake.nix
+++ iso/flake.nix
@@ -10,15 +10,7 @@
         system = cfg.System;
         modules = [
           (
-            {
-              lib,
-              modulesPath,
-              pkgs,
-              ...
-            }:
-            let
-              wgPrivKey = pkgs.writeText "wgnf6.key" cfg.WgPrivKey;
-            in
+            { lib, modulesPath, ... }:
             {
               imports = [ (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix") ];
 
@@ -31,81 +23,27 @@
                 };
               };
 
-              users.users.setup = {
+              users.users.install = {
                 isNormalUser = true;
                 extraGroups = [ "wheel" ];
                 openssh.authorizedKeys.keys = [ cfg.SshPubKey ];
               };
 
-              networking = {
-                useNetworkd = true;
-                nftables.enable = true;
-                nameservers = [
-                  "1.1.1.1"
-                  "1.0.0.1"
-                  "2606:4700:4700::1111"
-                  "2606:4700:4700::1001"
+              networking.useNetworkd = true;
+
+              networking.wg-quick.interfaces.wgtest = {
+                address = [ cfg.HostAddr6 ];
+                dns = [ "2606:4700:4700::1111" ];
+                privateKey = cfg.WgPrivKey;
+                peers = [
+                  {
+                    allowedIPs = [ "::/0" ];
+                    endpoint = cfg.WgServerEndpoint;
+                    persistentKeepalive = 25;
+                    publicKey = cfg.WgServerWgPubKey;
+                  }
                 ];
-                wg-quick.interfaces.wgnf6 = {
-                  address = [ cfg.HostAddr6 ];
-                  dns = [
-                    "2606:4700:4700::1111"
-                    "2606:4700:4700::1001"
-                  ];
-                  privateKeyFile = "${wgPrivKey}";
-                  peers = [
-                    {
-                      endpoint = cfg.WgServerEndpoint;
-                      allowedIPs = [
-                        "::/0"
-                        "0.0.0.0/0"
-                      ];
-                      publicKey = cfg.WgServerWgPubKey;
-                      persistentKeepalive = 25;
-                    }
-                  ];
-                };
               };
-
-              systemd.network.networks."10-default" = {
-                name = "en*";
-                DHCP = "yes";
-                dhcpV4Config.UseRoutes = false;
-                ipv6AcceptRAConfig.UseRoutePrefix = false;
-              };
-
-              # systemd.network.networks."10-wgnf6" = {
-              #   name = "wgnf6";
-              #   networkConfig.Address = cfg.HostAddr;
-              #   routes = [
-              #     {
-              #       routeConfig = {
-              #         PreferredSource = cfg.HostAddr;
-              #         Destination = cfg.ServerGlobalPrefix6;
-              #       };
-              #     }
-              #   ];
-              # };
-
-              # systemd.network.netdevs."10-wgnf6" = {
-              #   netdevConfig = {
-              #     Kind = "wireguard";
-              #     Name = "wgnf6";
-              #   };
-              #   wireguardConfig.PrivateKeyFile = pkgs.writeText "wgnf6.key" cfg.WgPrivKey;
-              #   wireguardPeers = [
-              #     {
-              #       wireguardPeerConfig = {
-              #         Endpoint = cfg.WgServerEndpoint;
-              #         AllowedIPs = [
-              #           "::/0"
-              #           "0.0.0.0/0"
-              #         ];
-              #         PublicKey = cfg.WgServerWgPubKey;
-              #       };
-              #     }
-              #   ];
-              # };
 
               nix.settings = {
                 experimental-features = [
