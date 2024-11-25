aee69d71b745ddf175e1668363ec72dcc20d2c42
Dane Rieber <danerieber@gmail.com>
2024-11-25 15:54:55 -0600

    sort geniso options

--- cli/client/geniso.go
+++ cli/client/geniso.go
@@ -9,21 +9,21 @@ import (
 var (
 	genisoHostAddr6           string
 	genisoServerGlobalPrefix6 string
-	genisoWgServerEndpoint    string
-	genisoWgServerWgPubKey    string
 	genisoSshPubKey           string
 	genisoSystem              string
 	genisoWgPrivKey           string
+	genisoWgServerEndpoint    string
+	genisoWgServerWgPubKey    string
 )
 
 func init() {
 	genisoCmd.Flags().StringVar(&genisoHostAddr6, "host-addr6", "", "host's IPv6 address")
-	genisoCmd.Flags().StringVar(&genisoWgServerEndpoint, "wg-server-endpoint", "auto", "WireGuard server's endpoint")
 	genisoCmd.Flags().StringVar(&genisoServerGlobalPrefix6, "server-global-prefix6", "auto", "Server's global IPv6 prefix")
-	genisoCmd.Flags().StringVar(&genisoWgServerWgPubKey, "wg-server-wg-pub-key", "auto", "WireGuard server's public key (if auto, will grab from server)")
 	genisoCmd.Flags().StringVar(&genisoSshPubKey, "ssh-pub-key", "", "your SSH public key")
 	genisoCmd.Flags().StringVar(&genisoSystem, "system", "", "host's system type (Nix system name)")
 	genisoCmd.Flags().StringVar(&genisoWgPrivKey, "wg-priv-key", "", "host's WireGuard private key")
+	genisoCmd.Flags().StringVar(&genisoWgServerEndpoint, "wg-server-endpoint", "auto", "WireGuard server's endpoint")
+	genisoCmd.Flags().StringVar(&genisoWgServerWgPubKey, "wg-server-wg-pub-key", "auto", "WireGuard server's public key (if auto, will grab from server)")
 }
 
 var genisoCmd = &cobra.Command{
@@ -48,11 +48,11 @@ var genisoCmd = &cobra.Command{
 		isoPath, err := iso.Generate("/tmp/nf6-geniso", &iso.Config{
 			HostAddr6:           genisoHostAddr6,
 			ServerGlobalPrefix6: genisoServerGlobalPrefix6,
-			WgServerEndpoint:    genisoWgServerEndpoint,
-			WgServerWgPubKey:    genisoWgServerWgPubKey,
 			SshPubKey:           genisoSshPubKey,
 			System:              genisoSystem,
 			WgPrivKey:           genisoWgPrivKey,
+			WgServerEndpoint:    genisoWgServerEndpoint,
+			WgServerWgPubKey:    genisoWgServerWgPubKey
 		})
 		if err != nil {
 			lib.Crash(err)
